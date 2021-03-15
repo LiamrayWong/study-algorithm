71605e21c2f0bf225922ff1dff414acd69cb4562
LiamrayWong <1079388320@qq.com>
2021-03-15 12:58:19 +0800

    feat: 链表刷题

---
+++ linkedlist/leetcode-142.cpp
@@ -0,0 +1,39 @@
+#include "common.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution
+{
+public:
+  ListNode *detectCycle(ListNode *head)
+  {
+    if (head == nullptr)
+      return nullptr;
+
+    ListNode *p = head, *q = head;
+    if (q->next == nullptr)
+      return nullptr;
+    do
+    {
+      p = p->next;
+      q = q->next->next;
+    } while (p != q && q && q->next);
+
+    if (q == nullptr || q->next == nullptr)
+      return nullptr;
+
+    //只能用while循环，用do-while循环会多走一次
+    p = head;
+    while (p != q)
+      p = p->next, q = q->next;
+
+    return q;
+  }
+};
--- linkedlist/linklist.cpp
+++ linkedlist/linklist.cpp
@@ -5,12 +5,20 @@ int next[10];
 
 //添加index节点添加节点p，值为value
 void add(int index, int p, int value)
-{
+{ //插入节点时有效，不插入时被覆盖
     next[p] = next[index];
+
     next[index] = p;
     data[p] = value;
     return;
 }
+
+struct Node
+{
+    Node(int data) : data(data), next(NULL) {}
+    int data;
+    Node *next;
+};
 
 int main()
 {
@@ -21,7 +29,8 @@ int main()
     add(5, 3, 2);
     add(3, 7, 3);
     add(7, 9, 4);
-
+    add(5, 4, 234);
+    printf("\n");
     //访问链表
     int p = head;
     while (p != 0)
